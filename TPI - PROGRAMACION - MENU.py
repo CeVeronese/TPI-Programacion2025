027567df0c596035bdbac38c6836c504c91c157c
Celeste Veronese <celesteveronese12@gmail.com>
2025-11-15 02:44:09 -0300

    MENU

---
+++ TPI - PROGRAMACION - MENU.py
@@ -0,0 +1,29 @@
+"""MENU"""
+print("""
+    🚌 𝐋𝐢𝐧𝐞𝐚𝐬 𝐃𝐢𝐬𝐩𝐨𝐧𝐢𝐛𝐥𝐞𝐬 🚌
+    🧡 C1-16 / 🩶 G1-16B
+    """)
+
+print("""
+    🚌🧡 𝐏𝐚𝐫𝐚𝐝𝐚𝐬 𝐝𝐞 𝐥𝐚 𝐋𝐢𝐧𝐞𝐚 𝐂𝟏-𝟏𝟔 🧡🚌
+    𝟏. Centro de Transferencias
+    𝟐. Av. Irigoyen / San Luis
+    𝟑. Bv. Alvear / Prol. Sarmiento
+    𝟒. Prol. Sarmiento / Av. Universidad
+    𝟓. Lacar / Aconcagua
+    𝟔. M.M. Moreno / Prol. Sarmiento
+    𝟕. Bv. Alvear / San Luis
+    𝟗. Lisandro de la Torre / Bs. As.
+      """)
+
+print("""
+      🚌🩶 𝐏𝐚𝐫𝐚𝐝𝐚𝐬 𝐝𝐞 𝐥𝐚 𝐋𝐢𝐧𝐞𝐚 𝐆𝟏-𝟏𝟔𝐁 🩶🚌
+      𝟏. Centro de Transferencias
+      𝟐. Bv Italia / Jujuy
+      𝟑. Gruta
+      𝟒. Terminal de Omnibús
+      𝟓. Bv Alvear / Mendoza
+      𝟔. Bv España / La Rioja
+      𝟕. Jauretche / Bv España (UNVM)
+      𝟗. Rawson / Bv España
+      """)
