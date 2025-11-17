facc436b567af7ab31d3036e2ec458d55d8a9fdc
José Missán <missanjose18@gmail.com>
2025-11-17 19:54:37 -0300

    Eliminacion de modulo innecesario

--- archivo1.py
+++ archivo1.py
@@ -1,13 +1,9 @@
-from datetime import datetime
-
 def mostrar_horarios_validos(paradas_dict, parada_user, hora_llegada):
-    hora_usuario = datetime.strptime(hora_llegada, "%H:%M").time()
     horarios_parada = paradas_dict[parada_user]
     horarios_validos = []
 
     for hora in horarios_parada:
-        hora_colectivo = datetime.strptime(hora, "%H:%M").time()
-        if hora_colectivo < hora_usuario:
+        if hora < hora_llegada:
             horarios_validos.append(hora)
 
     for hora in horarios_validos:
